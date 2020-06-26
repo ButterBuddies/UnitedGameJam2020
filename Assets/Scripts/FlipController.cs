c474b16cdb26a450b6145082a87ba32a0e2d9040
sarahcrichmond <sarah.richmond@medcognition.com>
2020-06-26 13:58:04 -0500

    Made the FlipGravityComponent which can be used to make objects gravity be "flipped" one the world is "flipped"
    Made the FlipGravityComponent which can be used to make objects gravity be "flipped" one the world is "flipped"

--- Assets/Scripts/FlipController.cs
+++ Assets/Scripts/FlipController.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class FlipController : MonoBehaviour
 {
     public GameObject flipAxis;
-    public GameObject yinGroup;
-    public GameObject yangGroup;
+    public GameObject yinGroupHolder;
+    public GameObject yangGroupHolder;
+    public GameObject[] yinGroup;
+    public GameObject[] yangGroup;
 
     private bool inFlipSequence;
-    private float flipValue = 1;
+    private float flipValue = -1;
     private float flipSpeed = 0.05f; //the smaller the slower the flip sequence
-    private int flip = 1;
+    private int flip = -1;
+
+    public void Start() //gets the yin and yang groups based on the group holders children in the hierarchy
+    {
+        yinGroup = new GameObject[yinGroupHolder.transform.childCount];
+
+        for (int i = 0; i < yinGroupHolder.transform.childCount; ++i)
+            yinGroup[i] = yinGroupHolder.transform.GetChild(i).gameObject;
+
+        yangGroup = new GameObject[yangGroupHolder.transform.childCount];
+
+        for (int i = 0; i < yangGroupHolder.transform.childCount; ++i)
+            yangGroup[i] = yangGroupHolder.transform.GetChild(i).gameObject;
+    }
+
+    public void FlipGravityOfGroups()
+    {
+        foreach (GameObject obj in yinGroup)
+        {
+            if (obj.GetComponent<FlipsGravity>())
+            {
+                obj.GetComponent<Rigidbody2D>().gravityScale = -obj.GetComponent<Rigidbody2D>().gravityScale;
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             flipAxis.gameObject.transform.localScale = new Vector3(1, flip, 1);
 
@@ -24,10 +50,12 @@ public class FlipController : MonoBehaviour
                 flip = -1;
             else
                 flip = 1;
+
+            FlipGravityOfGroups();
         }
 
         //pretty flip
-        if (Input.GetKeyDown(KeyCode.RightControl))
+        if (Input.GetKeyDown(KeyCode.RightShift))
         {
             //flipAxis.gameObject.transform.localScale = new Vector3(1, flip, 1);
             inFlipSequence = true;
@@ -40,7 +68,7 @@ public class FlipController : MonoBehaviour
 
         if (inFlipSequence)
         {
-            //updates pretyy flip over time (.5 seconds?)
+            //updates pretty flip over time (.5 seconds?)
             if (flip == 1)
             {
                 if (flipValue < 1)
@@ -67,6 +95,8 @@ public class FlipController : MonoBehaviour
                 }
 
             }
+
+            FlipGravityOfGroups();
         }
 
     }
