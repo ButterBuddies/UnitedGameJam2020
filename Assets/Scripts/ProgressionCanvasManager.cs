f62eac3eb6b5462b534e0dd9293bfbbd4d76e2c6
Gene Tigner <genetigner.art@gmail.com>
2020-06-28 01:27:38 -0500

    updated progression canvas manager script to show correct level numbers

--- Assets/Scripts/ProgressionCanvasManager.cs
+++ Assets/Scripts/ProgressionCanvasManager.cs
@@ -21,7 +21,7 @@ public class ProgressionCanvasManager : MonoBehaviour
         if(SceneManager.GetActiveScene().buildIndex != currentIndex)
         {
             currentIndex = SceneManager.GetActiveScene().buildIndex;
-            level = currentIndex % 6;
+            level = (currentIndex % 6) + 1;
             world = (int)((float)currentIndex / (float)5) + 1;
 
             //Debug.Log("World: " + world + " Level: " + level);
