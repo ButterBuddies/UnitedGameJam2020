ec34198180ca8f1d2dbf28c1d188f6e5f6b7c11b
Baumdiggity <atomnomicbomb@gmail.com>
2020-06-27 17:46:56 -0700

    Got rid of alot of fluff
    hopefully it won't break the game

---
+++ Assets/Scripts/ConstantRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+
+    private void Update()
+    {
+        this.gameObject.transform.Rotate(Vector3.forward * (rotationSpeed * Time.deltaTime));
+    }
+}
