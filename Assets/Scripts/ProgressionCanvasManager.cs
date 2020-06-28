64d9cd29ffd516559982de4012099ea8766b20ec
Gene Tigner <genetigner.art@gmail.com>
2020-06-28 00:37:32 -0500

    added world and level information for user

---
+++ Assets/Scripts/ProgressionCanvasManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ProgressionCanvasManager : MonoBehaviour
+{
+    public TextMeshProUGUI textToUpdate = null;
+    private int currentIndex = 0;
+    private int world = 0;
+    private int level = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex != currentIndex)
+        {
+            currentIndex = SceneManager.GetActiveScene().buildIndex;
+            level = currentIndex % 5;
+            world = (int)((float)currentIndex / (float)5) + 1;
+
+            //Debug.Log("World: " + world + " Level: " + level);
+            string str = "";
+            str +=$"World: {world} \t Level: {level} / 5";
+            textToUpdate.text = str;
+        }
+    }
+}
